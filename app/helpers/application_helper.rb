aa857c6a9cc4df24793a295b45510e356b5d08a3
Zeeshan Malik <zeeshan@zeeshan-VirtualBox.(none)>
2011-02-05 13:40:49 +0500

    layout_links tests added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,10 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  #Return logo for header
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
   
 end
